398919b1b6cbaac54000c6cca02863d2319f6255
anuann <anuann2000@gmail.com>
2022-05-06 01:01:53 +1200

    Speedserver code

---
+++ app.js
@@ -0,0 +1,18 @@
+var fs = require('fs'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+  if(req.url == "/"){
+    req.url="/index.html"
+  }
+  fs.readFile(__dirname +"/htmls"+ req.url, function (err, data) {
+    console.log(req.url);
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+}).listen(80);
---
+++ entrypoint.sh
@@ -0,0 +1,2 @@
+
+npm /SpeedServer/app.js
